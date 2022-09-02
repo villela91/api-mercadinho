123d9b19e705a5216c3f968e20784c9a3aa9104e
LUIZ VILLELA <106358282+villela91@users.noreply.github.com>
2022-09-02 02:08:21 -0300

    https://github.com/villela91/api-mercadinho

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const routes = require('./src/routes/corredor.route.js');
+const connectToDatabase = require('./src/dataBase/dataBase.js');
+
 const port = 3000;
 const app = express();
-const routes = require('./src/routes/corredor.route.js');
+
+connectToDatabase();
 
 app.use(express.json());
 app.use(cors());
